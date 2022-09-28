c5cef092378f4e503beb346afd309697a0e2d4e6
Şevval Bektaş <sevvalbektas5561@gmail.com>
2022-09-28 23:10:26 +0300

    last commit

--- weather.js
+++ weather.js
@@ -1,71 +1,113 @@
 const form = document.querySelector("section.top-banner form");
 const input = document.querySelector(".container input");
 const msg = document.querySelector("span.msg");
-const list = document.querySelector(".ajax-section .cities");
+const list = document.querySelector(".ajax-section ul.cities");
 
 // localStorage.setItem("tokenKey", "3cd7e5693312e483d80d44666bcb1851");
-localStorage.setItem("tokenKey", "Su8t+E/4HMKnjhQ2sBT+1+Oa/yedQHt4OVQGxG38wEV0tpkffpa5BcRDZa5ZWBQr");
 // localStorage.setItem("tokenKeyEncrypted", EncryptStringAES ("3cd7e5693312e483d80d44666bcb1851"))
+localStorage.setItem(
+  "tokenKey",
+  "Su8t+E/4HMKnjhQ2sBT+1+Oa/yedQHt4OVQGxG38wEV0tpkffpa5BcRDZa5ZWBQr"
+);
 
-form.addEventListener("submit", (event)=>{
-    event.preventDefault();
-    getWeatherDataFromApi();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  getWeatherDataFromApi();
 });
 
 //Get api func. (http methods == Verbs)
-const getWeatherDataFromApi = async() => {
-    // alert("http request is gone!");
-    const tokenKey = DecryptStringAES(localStorage.getItem("tokenKey"));
-    // const tokenKey = DecryptStringAES("Su8t+E/4HMKnjhQ2sBT+1+Oa/yedQHt4OVQGxG38wEV0tpkffpa5BcRDZa5ZWBQr");
-    // alert(tokenKey);
-    const inputValue = input.value;
-    const units = "metric";
-    const lang = "tr";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`;
-    try{
-    const response = await fetch(url).then(response=>response.json());
+const getWeatherDataFromApi = async () => {
+  // alert("http request is gone!");
+  const tokenKey = DecryptStringAES(localStorage.getItem("tokenKey"));
+  // const tokenKey = DecryptStringAES("Su8t+E/4HMKnjhQ2sBT+1+Oa/yedQHt4OVQGxG38wEV0tpkffpa5BcRDZa5ZWBQr");
+  // alert(tokenKey);
+  const inputValue = input.value;
+  const units = "metric";
+  const lang = "tr";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`;
+
+  try {
+    // const response = await fetch(url).then(response=>response.json());
+    const response = await axios(url);
     console.log(response);
-    const { main, sys,  weather, name } = response;
+    // const { main, sys,  weather, name } = response;
+    //obj destr.
+    const { main, sys, weather, name } = response.data;
 
     const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
     const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
 
     const cityNameSpans = list.querySelectorAll(".city span");
-    const cityNameSpansArray = Array.from(cityNameSpans); 
-    if(cityNameSpansArray.length > 0){
-        const filteredArray = cityNameSpansArray.filter(span => span.innerText == name);
-        if(filteredArray.length > 0 ){
-            msg.innerText = `You already know the weather for ${name}, Please search for another city 😉`;
-            setTimeout(() => {
-                msg.innerText = "";
-            }, 5000 )
-            form.reset();
-            return;
-        }
+    const cityNameSpansArray = Array.from(cityNameSpans);
+    if (cityNameSpansArray.length > 0) {
+      const filteredArray = cityNameSpansArray.filter(
+        (span) => span.innerText == name
+      );
+      if (filteredArray.length > 0) {
+        msg.innerText = `You already know the weather for ${name}, Please search for another city 😉`;
+        setTimeout(() => {
+          msg.innerText = "";
+        }, 5000);
+        form.reset();
+        return;
+      }
     }
     console.log(cityNameSpans);
     const createdLi = document.createElement("li");
     createdLi.classList.add("city");
-    createdLi.innerHTML = `<h2 class="city-name" data-name="${name}, ${sys.country}">
+    createdLi.innerHTML = `<h2 class="city-name" data-name="${name}, ${
+      sys.country
+    }">
                                 <span>${name}</span>
                                 <sup>${sys.country}</sup>
                             </h2>
-                            <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+                            <div class="city-temp">${Math.round(
+                              main.temp
+                            )}<sup>°C</sup></div>
                             <figure>
                                 <img class="city-icon" src="${iconUrl}">
-                                <figcaption>${weather[0].description}</figcaption>
+                                <figcaption>${
+                                  weather[0].description
+                                }</figcaption>
                             </figure>`;
     // append vs prepend
     list.prepend(createdLi);
-    form.reset();
-    }
-    catch (error) {
-        msg.innerText = `404 (City Not Found)`;
-        setTimeout(() => {
-            msg.innerText = "";
-        }, 5000 )
-        form.reset();
-    }
-}
 
+    //?capturing
+    // createdLi.addEventListener("click", (e) => {
+    //     if(e.target.tagName == "IMG"){
+    //         e.target.src = (e.target.src == iconUrl) ? iconUrlAWS : iconUrl;
+    //     }
+    // });
 
+    //?bubbling
+    // createdLi.addEventListener("click", (e) => {
+    //     alert(`Li element is clicked!`);
+    //     window.location.href = "https://clarusway.com";
+    // });
+    // createdLi.querySelector("figure").addEventListener("click", (e) => {
+    //     alert(`FIGURE element is clicked!`);
+    //     //! STOP BUBBLING
+    //     //e.stopPropagation();   // li'ye çıkmasını engeller
+    //     // window.location.href = "https://clarusway.com";
+    // });
+    // createdLi.querySelector("img").addEventListener("click", (e) => {
+    //     alert(`IMG element is clicked!`);
+    //     // window.location.href = "https://clarusway.com";
+    // });
+  } 
+  catch (error) {
+    msg.innerText = `404 (City Not Found)`;
+    setTimeout(() => {
+      msg.innerText = "";
+    }, 5000);
+  }
+  form.reset();
+};
+//window onload
+// document.querySelector(".cities").addEventListener("click", (e) => {
+//     if (e.target.tagName == "IMG") {
+//         alert("img is clicked!!!")
+//     }
+// }
+// )
